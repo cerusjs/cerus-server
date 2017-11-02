f78151ff700fc6fed06b9cfbcd810802d6980f6c
JortvD <jortvandriel@hotmail.com>
2017-11-02 18:26:46 +0100

    Added the package.json

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = function() {
+	var self = {};
+
+	var package = require("../package.json");
+	
+	self.name = package["name"];
+	self.version = package["version"];
+	self.dependencies = [
+		"cerus",
+		"cerus-promise",
+		"cerus-settings"
+	];
+
+	var server;
+
+	self.init_ = function(cerus) {
+		server = require("./lib/server")(cerus);
+	}
+
+	self.server = function() {
+		return server;
+	}
+
+	return self;
+}
